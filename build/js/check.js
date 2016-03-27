ff8e011572cb36e7550dfc5c095051e51adb508d
julia <glad198787@mail.ru>
2016-03-27 17:00:20 +0300

    module2-task1 FizzBuzz

--- build/js/check.js
+++ build/js/check.js
@@ -31,5 +31,34 @@ function getMessage(a, b){
     }
     return "Количество красных точек во всех строчках изображения: " + sum;
   }
+}
 
+/*
+* Write a program that prints the numbers from 1 to 100.
+* But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+* For numbers which are multiples of both three and five print “FizzBuzz”
+*/
+
+function append(value, tag){
+  var element = document.createElement('div');
+  element.innerHTML = value;
+  tag.appendChild(element);
+}
+
+function fizzBuzz(){
+  var div = document.createElement('div');
+  document.body.appendChild(div);
+  for (var i = 1; i <= 100; i++){
+    if((i % 3 == 0) && (i % 5 == 0)){
+      append("FizzBuzz", div);
+    }
+    if(i % 3 == 0){
+      append("Fizz", div);
+    }
+    if(i % 5 == 0){
+      append("Buzz", div);
+    } else{
+      append(i, div);
+    }
+  }
 }
